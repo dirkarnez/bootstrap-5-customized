c37276ae661b0dff81eafcbaa8985314a59cae14
dirkarnez <smalldirkalex@gmail.com>
2023-03-05 18:01:51 +0800

    - init, working

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Import our custom CSS
+import './mystyles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// import { Popover } from 'bootstrap';
+
+// // Create an example popover
+// document.querySelectorAll('[data-bs-toggle="popover"]')
+//   .forEach(popover => {
+//     new Popover(popover)
+//   })
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'assets/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            /*filename: '[hash].css'
+            to be customized
+            disable: !isProduction
+            */
+        })
+    ],
+    module: {
+        rules: [
+            {
+                mimetype: 'image/svg+xml',
+                scheme: 'data',
+                type: 'asset/resource',
+                generator: {
+                    filename: 'icons/[hash].svg'
+                }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
